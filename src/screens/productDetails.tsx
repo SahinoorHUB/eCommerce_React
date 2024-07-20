5a56953408b9a6f3c74c80da590b032125bbb898
SahinoorHUB <sahinoormd1996@gmail.com>
2024-07-20 10:00:06 +0530

    updated code 20.07.2024

--- src/screens/productDetails.tsx
+++ src/screens/productDetails.tsx
@@ -4,11 +4,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ProductService from "../services/product.service";
 import RatingShowing from "../component/rating/RatingShowing";
-import { IoIosCart } from 'react-icons/io';
+import { IoIosCart } from "react-icons/io";
+import CartAction from "../component/cartAction/cartAction";
+import cartStore from "../store/cart.store";
 
 const ProductDetails = () => {
   const { product_id } = useParams();
   const [getAllProductDetails, setAllProductDetails] = useState<any>(null);
+  const { addToCart, items } = cartStore();
 
   useEffect(() => {
     const fetchProduct = async () => {
@@ -24,6 +27,16 @@ const ProductDetails = () => {
     fetchProduct();
   }, [product_id]);
 
+  const handleAddItem = () => {
+    addToCart(getAllProductDetails);
+  };
+
+  const isCurrentItemInCart = () => {
+    return items.find(
+      (eachCartItem) => eachCartItem.product_id === getAllProductDetails.id
+    );
+  };
+
   return (
     <>
       {getAllProductDetails && (
@@ -31,39 +44,65 @@ const ProductDetails = () => {
           <div className="row">
             <div className="col-md-6 ">
               <div id="container">
-                <img className="card-img-top peoductImageBig" src={getAllProductDetails.image} /> 
+                <img
+                  className="card-img-top peoductImageBig"
+                  src={getAllProductDetails.image}
+                />
               </div>
             </div>
             <div className="col-md-6">
               <h3 className="gapping-top">{getAllProductDetails.title}</h3>
-              <p className="brand-list">Brand: {getAllProductDetails.category}</p>
-              <RatingShowing averageRating={Number(getAllProductDetails.rating.rate)} totalStars={5} fontSize={'20px'} fontSizeInfo={'15px'} />
+              <p className="brand-list">
+                Brand: {getAllProductDetails.category}
+              </p>
+              <RatingShowing
+                averageRating={Number(getAllProductDetails.rating.rate)}
+                totalStars={5}
+                fontSize={"20px"}
+                fontSizeInfo={"15px"}
+              />
               <p className="item-indicator">
                 <strong>About this item</strong>
               </p>
               <span className="a-list-item">
-                  {getAllProductDetails.description}
-                  </span>
+                {getAllProductDetails.description}
+              </span>
               <div className="price-div">
                 <b>Price:</b> &nbsp;
                 <span className="price-tag">
-                ₹ {getAllProductDetails.price}
+                  ₹ {getAllProductDetails.price}
                 </span>
               </div>
-              <div className="three-button">
-                <div className="product-customize card-add">
-                  <div className="btn btn-primary btn-sm btn-lg" style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
-                    <span className="addtocartIcon" style={{fontSize: '26px',  marginRight: '10px'}}> <IoIosCart></IoIosCart></span>
-                    Add To Cart
+
+              {!isCurrentItemInCart() ? (
+                <div className="three-button">
+                  <div className="product-customize card-add">
+                    <button
+                      className="btn btn-primary btn-sm btn-lg"
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        padding: "12px 21px"
+                      }}
+                      onClick={() => handleAddItem()}
+                    >
+                      <span
+                        className="addtocartIcon"
+                        style={{ fontSize: "26px", marginRight: "10px" }}
+                      >
+                        {" "}
+                        <IoIosCart></IoIosCart>
+                      </span>
+                      Add To Cart
+                    </button>
                   </div>
                 </div>
-                <div className="btn-sm-2">
-                  <div className="btn btn-primary btn-sm btn-lg">
-                    <i className="fas fa-heart"></i>
-                    Add To Wishlist
-                  </div>
+              ) : (
+                <div style={{marginTop: 17}}>
+                  <CartAction product_id={getAllProductDetails.id} />
                 </div>
-              </div> 
+              )}
             </div>
           </div>
         </div>
